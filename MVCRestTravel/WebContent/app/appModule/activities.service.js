254a9e437d91cfd0a34f74a3db9aff610af31ded
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-10-29 00:53:07 -0600

    created activities.service.js

---
+++ MVCRestTravel/WebContent/app/appModule/activities.service.js
@@ -0,0 +1,56 @@
+angular.module('appModule')
+.factory('activityService', function($http){
+	
+	var service = {};
+	
+	service.index = function(userId, destId){
+		return $http({
+		      method : 'GET',
+		      url : 'rest/users/'+ userId 
+		      + '/destinations/' + destId + '/activities'
+		    });
+	};
+	
+	service.show = function(userId, destId, activId){
+		return $http({
+		      method : 'GET',
+		      url : 'rest/users/'+ userId 
+		      + '/destinations' + destId + '/activities' + activeId
+		    });
+	};
+	
+	service.destroy = function(userId, destId, activId){
+		return $http({
+		      method : 'DELETE',
+		      url : 'rest/users/'+ userId 
+		      + '/destinations/' + destId + '/activities/' + activeId
+		    });
+	};
+	
+	service.create = function(userId, destId, destination){
+		return $http({
+		      method : 'POST',
+		      url : 'rest/users/'+ userId 
+		      +'/destinations/' + destId  +'/activities',
+		      headers : {
+		          'Content-Type' : 'application/json'
+		        },
+		      data : destination
+		    });
+	};
+	
+	service.update = function(userId, destId, activId, destination){
+		return $http({
+		      method : 'PUT',
+		      url : 'rest/users/' + userId 
+		      + '/destinations/' + destId + '/activities/' + activId,
+		      headers : {
+		          'Content-Type' : 'application/json'
+		        },
+		      data : destination
+		    });
+	};
+	
+	return service;
+
+});
